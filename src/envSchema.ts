e48a5e56fb764ce6e0f683f59fbad6336a054e2f
linventif <gregoire.becue@proton.me>
2025-09-10 15:44:38 +0200

    feat: add DOWNLOAD_PATH

--- src/envSchema.ts
+++ src/envSchema.ts
@@ -9,6 +9,7 @@ export const envSchema = z.object({
 		.optional(),
 	AUTH_USERNAME: z.string().nonempty('AUTH_USERNAME is required'),
 	AUTH_PASSWORD: z.string().nonempty('AUTH_PASSWORD is required'),
+	DOWNLOAD_PATH: z.string().nonempty('DOWNLOAD_PATH is required'),
 });
 
 // Validate the environment variables
